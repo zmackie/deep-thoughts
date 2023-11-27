8f0c5c2f5b6123905f68ea38e53da13f07a3349d
Zander Mackie <zmackie@gmail.com>
2023-11-27 12:47:07 -0500

    lesson 4

--- posts/lesson 4/Untitled-1.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-docker run --pull always -it -v /root/scrape/data-disk:/var/scrape --entrypoint /bin/bash --network selenium registry.digitalocean.com/brokernomics/brokerscraper:fuck-latest
